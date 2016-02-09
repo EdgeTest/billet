00a5bb829eff55b82472bab6427cd7504b65cc5a
SpencerJB <spencer.booth@zen.co.uk>
2016-02-09 11:15:18 +0000

    Comment all the types.

--- Billet.Domain/Entities/SubscriptionType.cs
+++ Billet.Domain/Entities/SubscriptionType.cs
@@ -4,8 +4,14 @@ namespace Billet.Domain.Entities
     /// The types of subscription a customer may have.</summary>
     public enum SubscriptionType
     {
+        /// <summary>
+        /// The TV subscription type.</summary>
         TV,
+        /// <summary>
+        /// The Phone subscription type.</summary>
         Talk,
+        /// <summary>
+        /// The internet subscription type.</summary>
         Broadband
     }
 }
